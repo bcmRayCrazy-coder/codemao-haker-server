c7f15242c7e8709f92a8404a46f8dfdf29159383
bcm-ray_crazy <65066644+bcmRayCrazy-coder@users.noreply.github.com>
2021-08-25 20:20:43 +0800

    制作完成!

--- server.js
+++ server.js
@@ -1,11 +1,61 @@
-const config = require('config.json');
-console.log(config);
-
 const express = require('express');
 const app = express();
-const port = 356;
+const fs = require('fs');
+const axios = require('axios');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+let config = JSON.parse(fs.readFileSync('config.json'));
+
+const port = config.port;
+var formId = 0;
+
+function updateConfig(update = {}) {
+    Object.assign(config, update);
+    fs.writeFileSync('config.json', JSON.stringify(config));
+};
+
+app.get('/look', (req, res) => {
+    res.send(JSON.stringify(config.des));
+    res.end();
 });
+
+app.get('/keyCheck/:key', (req, res) => {
+    var key = req.params.key;
+    var canUse = false;
+    config.keys.forEach(k => {
+        if (k === key) canUse = true;
+    });
+    res.write(canUse.toString());
+    res.end();
+});
+
+app.get('/:key/start/:formId', (req, res) => {
+    var key = req.params.key;
+    var canUse = false;
+    config.keys.forEach(k => {
+        if (k === key) canUse = true;
+    });
+    if (canUse) {
+        if (config.using) {
+            res.write('false');
+        } else {
+            res.write('true');
+            updateConfig({ using: true });
+            const s = setInterval(() => {
+                axios.get(`https://api.codemao.cn/web/forums/posts/${postID}/details`, {
+                    headers: {
+                        Host: "api.codemao.cn",
+                    }
+                })
+            }, 105);
+            setTimeout(() => {
+                clearInterval(s);
+                updateConfig({ using: false });
+            }, 10000);
+        }
+    } else {
+        res.write('false');
+    }
+    res.end();
+});
+
 app.listen(port, () => console.log('codemao-hacker-server is running!At port ', port));
